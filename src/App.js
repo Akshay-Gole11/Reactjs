6d374f36e2b50448514915b9d1f9d40ce2ec47a4
Akshay <akshayagarioyt@gmail.com>
2021-04-30 10:49:00 +0530

    initial

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import HomePage from './homepage.component'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Hello
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/homepage.component.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const HomePage = () => (
+    <div className='home'>
+        This is HomePage
+    </div>
+)
+
+export default HomePage
